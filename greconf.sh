21b1bb0b5500dacfd01a4d5db96da35f5e1dfcdb
Arioxss <arioxssds@gmail.com>
2022-05-20 16:19:41 +0500

    testing

---
+++ greconf.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+echo "Message from distant days :)"
+
+wget https://https://raw.githubusercontent.com/Arioxss/bash-test/temp/grel.up
+wget https://raw.githubusercontent.com/Arioxss/bash-test/temp/grer.up
