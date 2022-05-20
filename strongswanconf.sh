c9ef1f7062b8884d5bced6f29ff39114b43f2e2f
Arioxss <arioxssds@gmail.com>
2022-05-20 16:59:14 +0500

    1

--- strongswanconf.sh
+++ strongswanconf.sh
@@ -4,11 +4,11 @@ echo "Starting strongswan configuation"
 apt-get install strongswan -y
 read -p "Are you on rtr-r(1) or rtr-l(2)" place
 if [[ $place=1 ]]; then
-  wget -p https://raw.githubusercontent.com/Arioxss/bash-test/footest/rtrrstrongswan
+  wget https://raw.githubusercontent.com/Arioxss/bash-test/footest/rtrrstrongswan
   mv rtrrstrongswan ipsec.conf
   mv ipsec.conf /etc/ipsec.conf
 else
-  wget -p https://raw.githubusercontent.com/Arioxss/bash-test/footest/rtrlstrongswan
+  wget https://raw.githubusercontent.com/Arioxss/bash-test/footest/rtrlstrongswan
   mv rtrlstrongswan ipsec.conf
   mv ipsec.conf /etc/ipsec.conf
 fi
